417db9b62468fd58ed02ff951b7f1c6f3f5eb9d7
heruramdani <hermdn16@gmail.com>
2023-03-21 10:03:37 +0700

    Pert 3 Slice

---
+++ lib/pages/get_started_page.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'main_menu.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/img-started.png"))),
+        ),
+        Container(
+          height: 444,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black.withOpacity(0.9),
+              Colors.black.withOpacity(0.0)
+            ],
+          )),
+        ),
+        SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Treat ',
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        color: const Color(0xffFFFFFF),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'Patient ',
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        color: const Color(0xff308CF8),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'Like A ',
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        color: const Color(0xffFFFFFF),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'Queen',
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        color: const Color(0xff308CF8),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Text(
+                  'Explore the best our services with \nprofessional specialist doctor',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: const Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w300,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MainMenuPage()),
+                  );
+                },
+                child: Center(
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 80),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 48, vertical: 14),
+                    decoration: BoxDecoration(
+                        color: const Color(0xffFFFFFF),
+                        borderRadius: BorderRadius.circular(17)),
+                    child: Text(
+                      'Get Started',
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        color: const Color(0xff111111),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+}
---
+++ lib/pages/main_menu.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MainMenuPage extends StatelessWidget {
+  const MainMenuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget doctorCard(
+      String img,
+      String name,
+      String role,
+    ) {
+      return Container(
+        padding: const EdgeInsets.all(10),
+        margin: EdgeInsets.only(bottom: 16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(17),
+            color: const Color(0xffFFFFFF)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 24,
+            ),
+            Container(
+              height: 220,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(17),
+                  image: DecorationImage(
+                      fit: BoxFit.cover, image: AssetImage(img))),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          color: const Color(0xff111111),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        role,
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: const Color(0xff111111),
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage("assets/ic-star.png"))),
+                      ),
+                      Text(
+                        '4.8',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: const Color(0xff308CF8),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+        backgroundColor: const Color(0xffFAFAFA),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hello, \nHeru Ramdani',
+                            style: GoogleFonts.poppins(
+                              fontSize: 24,
+                              color: const Color(0xff111111),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Stay Healthy with Our Spesialis Doctor',
+                            style: GoogleFonts.poppins(
+                              fontSize: 14,
+                              color: const Color(0xff9698A9),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 76,
+                        height: 76,
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage("assets/img-profile.png"))),
+                      )
+                    ],
+                  ),
+                  doctorCard("assets/img-doc1.png", "Dr Mawar N",
+                      "Specialist penyakit dalam"),
+                  doctorCard("assets/img-doc2.png", "Dr Zizah Ayuningsih",
+                      "Specialist kulit kelamin")
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
